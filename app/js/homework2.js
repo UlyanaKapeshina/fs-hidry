0416bce54bac2e1139e86ca6de0ab247afefc942
Ulyana Kapeshina <ulyanakapeshina@gmail.com>
2020-12-11 17:43:21 +0300

    добавление подсветки рандомным элементам каждые 2с и удаление подсветки с рандомных элементов каждые 2с с помощью дата-аттрибутов

--- app/js/homework2.js
+++ app/js/homework2.js
@@ -1,48 +1,53 @@
 const INTERVAL = 2000;
 const COLOR_RANGE_MAX = 255;
-const elements = document.body.querySelectorAll(":not(script)");
+const elements = document.body.querySelectorAll("*");
 
 const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
 
 const getRandomElement = (elementsList) => {
-  const element = elementsList[getRandomInt(elementsList.length - 1)];
-  return element;
-};
-let style = document.querySelector("style");
-
-if (!style) {
-  const newStyle = document.createElement("style");
-  document.querySelector("head").appendChild(newStyle);
-  style = document.querySelector("style");
-}
-
-const generateColorRanges = () => {
-  const red = getRandomInt(255);
-  const green = getRandomInt(255);
-  const blue = getRandomInt(255);
-  return [red, green, blue];
+  return elementsList[getRandomInt(elementsList.length - 1)];
 };
 
-const addBackgroundClass = (element) => {
-  const [red, green, blue] = generateColorRanges();
-  const bgClassName = `bgcolor-${red}${green}${blue}`;
-  style.textContent = `${style.textContent}\n.${bgClassName} {\nbackground-color: rgb(${red}, ${green}, ${blue});\n}`;
-  element.classList.add(bgClassName);
+const generateColor = () => {
+  const red = getRandomInt(COLOR_RANGE_MAX);
+  const green = getRandomInt(COLOR_RANGE_MAX);
+  const blue = getRandomInt(COLOR_RANGE_MAX);
+  return `rgb(${red}, ${blue}, ${green})`;
 };
-const removeBackgroundClass = (element) => {
-  Array.from(element.classList)
-    .filter((it) => it.includes("bgcolor"))
-    .forEach((it) => {
-      element.classList.remove(it);
-      const regexp = new RegExp(`\n\\.${it} \\{[^\\{\\}]*\\}`);
-      style.textContent = style.textContent.replace(regexp, "");
-    });
+
+const addBackgroundColor = (element) => {
+  const bgNew = generateColor();
+  const bgCurrent = element.style.backgroundColor;
+  if (bgCurrent) {
+    element.dataset.bgPrev = bgCurrent;
+  }
+  element.dataset.bgNew = bgNew;
+  element.style.backgroundColor = bgNew;
+};
+
+const removeBackgroundColor = (element) => {
+  const bgNew = element.dataset.bgNew;
+  if (!bgNew) {
+    return;
+  }
+  const bgPrev = element.dataset.bgPrev;
+  const bgCurrent = element.style.backgroundColor;
+  element.removeAttribute("data-bg-new");
+  element.removeAttribute("data-bg-prev");
+  if (bgNew !== bgCurrent) {
+    return;
+  }
+  if (bgPrev) {
+    element.style.backgroundColor = bgPrev;
+    return;
+  }
+  element.style.backgroundColor = "";
 };
 
 setInterval(() => {
-  addBackgroundClass(getRandomElement(elements));
+  addBackgroundColor(getRandomElement(elements));
 }, INTERVAL);
 
 setInterval(() => {
-  removeBackgroundClass(getRandomElement(elements));
+  removeBackgroundColor(getRandomElement(elements));
 }, INTERVAL);
